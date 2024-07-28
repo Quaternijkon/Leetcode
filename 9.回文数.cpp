fef6880b1c5b72ae618be41d1f912dbffc9b968f
dry <1729361922@qq.com>
2024-07-28 17:42:54 +0800

    update

--- 9.回文数.cpp
+++ 9.回文数.cpp
@@ -5,10 +5,34 @@
  */
 
 // @lc code=start
+#include <math.h>
+using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
+        if(x < 0) return false;
+        int temp = x;
+        long long res = 0;
+        while(x){
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return res == temp;
+    }
 
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int len = log10(x) + 1;
+        int num = x;
+        while (num) {
+            int left = num / pow(10, len - 1);
+            int right = num % 10;
+            if (left != right) return false;
+            num %= static_cast<int>(pow(10, len - 1));
+            num /= 10;
+            len -= 2;
+        }
+        return true;
     }
 };
 // @lc code=end
